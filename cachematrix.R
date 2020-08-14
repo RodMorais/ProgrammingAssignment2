86b7b9fbb72d5667345cd78999f2431530e0e65d
RodMorais <rmorais@gmail.com>
2020-08-14 20:59:18 -0300

    3rd commit, cleaning up

--- cachematrix.R
+++ cachematrix.R
@@ -14,11 +14,8 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   set.solve <- function(solve) s <<- solve
   get.solve <- function() s
-  list(set = set, get = get,
-       set.solve = set.solve,
-       get.solve = get.solve)
+  list(set = set, get = get, set.solve = set.solve, get.solve = get.solve)
 }
-
 
 ## Write a short comment describing this function
 # Returning a matrix that is the inverse of 'x'
@@ -33,4 +30,4 @@ cacheSolve <- function(x, ...) {
   s <- solve(data, ...)
   x$set.solve(s)
   s
-} 
+}
